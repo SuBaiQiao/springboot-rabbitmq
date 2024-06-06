0cfd7ca6d08f0f73da5dcb52be8f10641755a82c
曹朝禹 <1294749811@qq.com>
2024-06-06 17:24:28 +0800

    生产者发送信息

---
+++ mq-provider/src/main/java/com/subaiqiao/mqProvider/controller/SendMessageController.java
@@ -0,0 +1,31 @@
+package com.subaiqiao.mqProvider.controller;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/messages")
+public class SendMessageController {
+
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    @GetMapping("/send")
+    public String sendMessage() {
+        String messageId = UUID.randomUUID().toString();
+        String message = "这是测试内容";
+        Map<String, Object> map = new HashMap<>();
+        map.put("messageId", messageId);
+        map.put("message", message);
+        map.put("sendTime", new Date());
+        rabbitTemplate.convertAndSend("TestDirectExchange", "TestDirectRouting", map);
+        return "ok";
+    }
+}
---
+++ mq-provider/src/main/java/com/subaiqiao/mqProvider/exchange/direct/DirectRabbitConfig.java
@@ -0,0 +1,50 @@
+package com.subaiqiao.mqProvider.exchange.direct;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 直连型交换机
+ */
+@Configuration
+public class DirectRabbitConfig {
+
+    /**
+     * TestDirectQueue队列
+     * @return 队列
+     */
+    @Bean
+    public Queue TestDirectQueue() {
+        // durable：是否持久化，默认是false。持久化队列：会被存储在磁盘上，当消息代理重启之后，依然会存在。暂存队列：当前连接有效
+        // exclusive：默认是false。只能在当前创建的连接使用，关闭连接后删除。优先级高于durable。
+        // autoDelete：是否自动删除，当没有生产者或消费者使用此队列，该队列会自动删除。
+
+        // 一般情况下设置为持久话即可，剩下两个都是false。
+        System.out.println("创建队列");
+        return new Queue("TestDirectQueue", false);
+    }
+
+    /**
+     * TestDirectExchange 交换机
+     * @return 交换机
+     */
+    @Bean
+    DirectExchange TestDirectExchange() {
+        System.out.println("创建交换机");
+        return new DirectExchange("TestDirectExchange", true, false);
+    }
+
+    /**
+     * 将队列和交换机绑定到路由，设置匹配键TestDirectRouting
+     * @return 绑定
+     */
+    @Bean
+    Binding bindingDirect() {
+        System.out.println("路由绑定");
+        return BindingBuilder.bind(TestDirectQueue()).to(TestDirectExchange()).with("TestDirectRouting");
+    }
+}
