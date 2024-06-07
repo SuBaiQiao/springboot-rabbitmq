03bc107a08a0b221c6f382945ff0de21fb77b74a
曹朝禹 <1294749811@qq.com>
2024-06-07 11:26:04 +0800

    回调

--- mq-provider/src/main/java/com/subaiqiao/mqProvider/config/RabbitMQConfig.java
+++ mq-provider/src/main/java/com/subaiqiao/mqProvider/config/RabbitMQConfig.java
@@ -1,5 +1,7 @@
 package com.subaiqiao.mqProvider.config;
 
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.DefaultClassMapper;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
@@ -9,6 +11,30 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
+    @Bean
+    public RabbitTemplate createRabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate();
+        rabbitTemplate.setConnectionFactory(connectionFactory);
+        // 开启之后才可以触发回调
+        rabbitTemplate.setMandatory(true);
+        rabbitTemplate.setMessageConverter(jsonToMapMessageConverter());
+
+        rabbitTemplate.setConfirmCallback((correlationData, ack, cause) -> {
+            System.out.println("ConfirmCallback相关数据：" + correlationData);
+            System.out.println("ConfirmCallback确认情况：" + ack);
+            System.out.println("ConfirmCallback原因：" + cause);
+        });
+
+        rabbitTemplate.setReturnsCallback(returnedMessage -> {
+            System.out.println("ReturnsCallback消息：" + returnedMessage.getMessage());
+            System.out.println("ReturnsCallback回应码：" + returnedMessage.getReplyCode());
+            System.out.println("ReturnsCallback回应消息：" + returnedMessage.getReplyText());
+            System.out.println("ReturnsCallback交换机：" + returnedMessage.getExchange());
+            System.out.println("ReturnsCallback路由键：" + returnedMessage.getRoutingKey());
+        });
+
+        return rabbitTemplate;
+    }
 
     @Bean
     public MessageConverter jsonToMapMessageConverter() {
@@ -18,4 +44,5 @@ public class RabbitMQConfig {
         jackson2JsonMessageConverter.setClassMapper(defaultClassMapper);
         return jackson2JsonMessageConverter;
     }
+
 }
